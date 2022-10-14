affad8e5b42f12b3c5d71a9f04b1d4df6b0be664
VinzentPilz <81297495+VinzentPilz@users.noreply.github.com>
2022-10-14 14:54:16 +0200

    Background subtraction slider

---
+++ Video/background_subtraction_slider/background_subtraction_slider.py
@@ -0,0 +1,47 @@
+from cv2 import threshold
+import numpy as np
+import cv2
+
+#Video aus Datei öffnen
+cap = cv2.VideoCapture("Micro-dance_2_.wm")
+
+threshold = 26
+frameCount = 0
+
+#FCallback-Funktion für den Slider
+def changeThreshold(trackbarValue):
+    global threshold
+    threshold = trackbarValue
+    
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    #Video in Graustufen wandeln
+    mask = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    if frameCount == 0:
+        #Wichtig Aufruf copy(): firstFrame = mask würde keine Pixel kopieren 
+        firstFrame = mask.copy()
+        cv2.imshow("Background Model", firstFrame)
+
+        #Slider
+        cv2.createTrackbar("Threshold", "Background Model", 0, 127, changeThreshold)
+        cv2.setTrackbarPos("Threshold", "Background Model", threshold)
+
+        frameCount += 1
+
+    #aktuelles Frame vom Hintergrund subtrahieren
+    cv2.absdiff(mask, firstFrame, mask)
+
+    #Schwellwertbildung
+    ret, mask = cv2.threshold(mask, threshold, 255, cv2.THRESH_BINARY)
+
+    #Maske darstellen
+    cv2.imshow("Video", mask)
+
+    if cv2.waitKey(25) != -1:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
